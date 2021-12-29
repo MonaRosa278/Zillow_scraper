e1a6e4b59b4414427ac6ff88f69843d5e14ab18d
MonaRosa <68399465+MonaRosaWang@users.noreply.github.com>
2021-12-29 12:40:18 -0800

    Add files via upload

--- zillowScraper.py
+++ zillowScraper.py
@@ -1,105 +1,103 @@
 '''
-Author: rosa wang 
+Author: mona rosa
 Date: 2021-02-16 16:53:57
 LastEditTime: 2021-02-16 19:00:26
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /ten_apps/Users/rosawang/Desktop/cs_application/resume_projects/zillowScraper.py
 '''
+
+
+from lxml import html
 import requests
-from bs4 import BeautifulSoup
-import csv
+import unicodecsv as csv
+import argparse
 
+def parse(zipcode,filter=None):
 
-class ZillowScraper:
-    results = []
+	if filter=="newest":
+		url = "https://www.zillow.com/homes/for_sale/{0}/0_singlestory/days_sort".format(zipcode)
+	elif filter == "cheapest":
+		url = "https://www.zillow.com/homes/for_sale/{0}/0_singlestory/pricea_sort/".format(zipcode)
+	else:
+		url = "https://www.zillow.com/homes/for_sale/{0}_rb/?fromHomePage=true&shouldFireSellPageImplicitClaimGA=false&fromHomePageTab=buy".format(zipcode)
+	
+	for i in range(5):
+		# try:
+		headers= {
+					'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+					'accept-encoding':'gzip, deflate, sdch, br',
+					'accept-language':'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+					'cache-control':'max-age=0',
+					'upgrade-insecure-requests':'1',
+					'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+		}
+		response = requests.get(url,headers=headers)
+		print(response.status_code)
+		parser = html.fromstring(response.text)
+		search_results = parser.xpath("//div[@id='search-results']//article")
+		properties_list = []
+		
+		for properties in search_results:
+			raw_address = properties.xpath(".//span[@itemprop='address']//span[@itemprop='streetAddress']//text()")
+			raw_city = properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressLocality']//text()")
+			raw_state= properties.xpath(".//span[@itemprop='address']//span[@itemprop='addressRegion']//text()")
+			raw_postal_code= properties.xpath(".//span[@itemprop='address']//span[@itemprop='postalCode']//text()")
+			raw_price = properties.xpath(".//span[@class='zsg-photo-card-price']//text()")
+			raw_info = properties.xpath(".//span[@class='zsg-photo-card-info']//text()")
+			raw_broker_name = properties.xpath(".//span[@class='zsg-photo-card-broker-name']//text()")
+			url = properties.xpath(".//a[contains(@class,'overlay-link')]/@href")
+			raw_title = properties.xpath(".//h4//text()")
+			
+			address = ' '.join(' '.join(raw_address).split()) if raw_address else None
+			city = ''.join(raw_city).strip() if raw_city else None
+			state = ''.join(raw_state).strip() if raw_state else None
+			postal_code = ''.join(raw_postal_code).strip() if raw_postal_code else None
+			price = ''.join(raw_price).strip() if raw_price else None
+			info = ' '.join(' '.join(raw_info).split()).replace(u"\xb7",',')
+			broker = ''.join(raw_broker_name).strip() if raw_broker_name else None
+			title = ''.join(raw_title) if raw_title else None
+			property_url = "https://www.zillow.com"+url[0] if url else None 
+			is_forsale = properties.xpath('.//span[@class="zsg-icon-for-sale"]')
+			properties = {
+							'address':address,
+							'city':city,
+							'state':state,
+							'postal_code':postal_code,
+							'price':price,
+							'facts and features':info,
+							'real estate provider':broker,
+							'url':property_url,
+							'title':title
+			}
+			if is_forsale:
+				properties_list.append(properties)
+		return properties_list
+		# except:
+		# 	print ("Failed to process the page",url)
 
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-        'cache-control': 'max-age=0',
-        'cookie': 'zguid=23|%24130fad5f-ec73-46ab-8b08-164f5e600d51; zgsession=1|d109a0bd-aaea-4327-ba8b-6a1e07e101a9; zjs_anonymous_id=%22130fad5f-ec73-46ab-8b08-164f5e600d51%22; _ga=GA1.2.1366003292.1606196308; _gid=GA1.2.1621946796.1606196308; _gcl_au=1.1.2119618559.1606196308; KruxPixel=true; DoubleClickSession=true; _pxvid=46fcba6e-2e17-11eb-a3bb-0242ac120008; JSESSIONID=ADA6C199E18AAC44C03E859DBE1A4BFA; _fbp=fb.1.1606196307815.44750306; _pin_unauth=dWlkPU4yRmxZalV5WldZdFlUbGpNaTAwTURSbUxUbGtNRGN0T1RGbVpqWTVNRFJoTjJSaw; __gads=ID=fb7d66100d34c50c:T=1606196318:S=ALNI_MaeGOWKvq7t0fptEWfEMHEcXt4KgQ; ki_r=; KruxAddition=true; ki_s=; G_ENABLED_IDPS=google; loginmemento=1|7f99ce28916589aaa34b3bd3bb773bfe65b608aad0bdc39e168bc26704a17432; ZILLOW_SSID=1|AAAAAVVbFRIBVVsVEtZM9%2F7vxMX883J5K%2B68Jsx39kNluUUAzIycx57o1Zqf5Gcacp9M%2F0Fkao1tWZ4SkBTImii22u5d; userid=X|3|68effc57631fe0cc%7C2%7ClGLIm7LVZ_QSJAnmTM0L8HLC4XhQtVuUr1Iuq2L-RXg%3D; ZILLOW_SID=1|AAAAAVVbFRIBVVsVEg0sVoK2%2FSfVoMAeri9zCxGSi5K8eoNgu8PqUFLxexzdurCt%2Bf6ob7nDqG4abAH8DN0JAVHYHgsB; zjs_user_id=%22X1-ZUtj1ki00qcvm1_2599o%22; optimizelyEndUserId=oeu1606201443171r0.9810999660065434; FSsampler=1267573903; _px3=0c8c5c98e7e3118904fadf5e5ab5663e1af3ff48b2bec4ce0e35375d4d988c6e:ZYOK+qlgTxGFd43nL/GqxypGsg+AGKKC3t03zjpkGM8bkfsCGiqOzEieNE1dkkqZZkRwv+m93C/43F4bsZABmg==:1000:IMxftrXcTjULXAolEeOuGBH7GbrnsIh5MbDqDZk08vErGn9fcvn2UgoVI37//vO0RUEb1NhmiAcMbc1Nxmp+hVcxmUTbOJoe0jKtxVvgfp/MNv5XWVZj/oHY0EEH53Lh2OJdAT+YzoaaOHHsRaSMiK9lOJfNLJXcvkyLpLp3Gkg=; _uetsid=4720a3502e1711eb809c13d79d4144d4; _uetvid=4720c0002e1711ebb98ac5cc20cd7896; ki_t=1606196318513%3B1606196318513%3B1606205078467%3B1%3B52; AWSALB=V/JTHrfSJGECKbS2gi+51/fdX3dXtU4LHLzI9O7Rg+FX9vva0TOb3RdwbEoWFQpuZMZI0iD5MteCUzlMpY8clTe29ZsqqxfmlCtMaammVQNRZOoP2RtByAWvlext; AWSALBCORS=V/JTHrfSJGECKbS2gi+51/fdX3dXtU4LHLzI9O7Rg+FX9vva0TOb3RdwbEoWFQpuZMZI0iD5MteCUzlMpY8clTe29ZsqqxfmlCtMaammVQNRZOoP2RtByAWvlext; search=6|1608797078502%7Crect%3D37.463887656692116%252C-122.00925505236816%252C37.34347864154035%252C-122.15327894763183%26rid%3D32999%26disp%3Dmap%26mdm%3Dauto%26p%3D1%26sort%3Ddays%26z%3D1%26type%3Dhouse%252Ccondo%252Ctownhouse%252Capartment%26fs%3D0%26fr%3D1%26mmm%3D0%26rs%3D0%26ah%3D0%26singlestory%3D0%26housing-connector%3D0%26abo%3D0%26garage%3D0%26pool%3D0%26ac%3D0%26waterfront%3D0%26finished%3D0%26unfinished%3D0%26cityview%3D0%26mountainview%3D0%26parkview%3D0%26waterview%3D0%26hoadata%3D1%26zillow-owned%3D0%263dhome%3D0%09%0932999%09%09%09%09%09%09; _gat=1',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
-    }
+if __name__=="__main__":
+	argparser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+	argparser.add_argument('zipcode',help = '')
+	sortorder_help = """
+    available sort orders are :
+    newest : Latest property details,
+    cheapest : Properties with cheapest price
+    """
+	argparser.add_argument('sort',nargs='?',help = sortorder_help,default ='Homes For You')
+	args = argparser.parse_args()
+	zipcode = args.zipcode
+	sort = args.sort
+	print ("Fetching data for %s"%(zipcode))
+	scraped_data = parse(zipcode,sort)
+	print ("Writing data to output file")
+	with open("properties-%s.csv"%(zipcode),'wb')as csvfile:
+		fieldnames = ['title','address','city','state','postal_code','price','facts and features','real estate provider','url']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for row in  scraped_data:
+			writer.writerow(row)
 
-    def fetch(self, url, params):
-        print('HTTP GET request to URL: %s' % url, end='')
-        res = requests.get(url, params=params, headers=self.headers)
-        print(' | Status code: %s' % res.status_code)
-        
-        return res
-   
-    def save_response(self, res):
-        with open('res.html', 'w') as html_file:
-            html_file.write(res)
-
-    def load_response(self):
-        html = ''
-        
-        with open('res.html', 'r') as html_file:
-            for line in html_file:
-                html += line
-        
-        return html
-   
-    def parse(self, html):
-        content = BeautifulSoup(html, 'lxml')
-        cards = content.findAll('article', {'class': 'list-card'})
-        
-        for card in cards:
-            try:
-                ba = card.find('ul', {'class': 'list-card-details'}).findAll('li')[1].text.split(' ')[0]
-            except:
-                ba = 'N/A'
-            
-            try:
-                sqft = card.find('ul', {'class': 'list-card-details'}).findAll('li')[2].text.split(' ')[0]
-            except:
-                sqft = 'N/A'
-            
-            try:
-                image = card.find('img')['src']
-            except:
-                image = 'N/A'
-
-            self.results.append({
-                'price': card.find('div', {'class': 'list-card-price'}).text,
-                'address': card.find('address', {'class': 'list-card-addr'}).text,
-                'bds': card.find('ul', {'class': 'list-card-details'}).findAll('li')[0].text.split(' ')[0],
-                'ba': ba,
-                'sqft': sqft,
-                'image': image
-            })
-    
-    def to_csv(self):
-        with open('zillow1.csv', 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=self.results[0].keys())
-            writer.writeheader()
-            
-            for row in self.results:
-                writer.writerow(row)
-        
-    def run(self):
-        for page in range(1, 5):
-            params = {
-                searchQueryState: {"pagination":{},"usersSearchTerm":"mountain view, ca","mapBounds":{"west":-122.15327894763183,"east":-122.00925505236816,"south":37.34347864154035,"north":37.463887656692116},"regionSelection":[{"regionId":32999,"regionType":6}],"isMapVisible":true,"filterState":{"fsba":{"value":false},"fsbo":{"value":false},"nc":{"value":false},"fore":{"value":false},"cmsn":{"value":false},"auc":{"value":false},"pmf":{"value":false},"pf":{"value":false},"fr":{"value":true},"ah":{"value":true}},"isListVisible":true,"mapZoom":13}" %page}
-            }
-            
-
-            res = self.fetch('https://www.zillow.com/mountain-view-ca/rentals/', params)
-            self.parse(res.text)
-
-        self.to_csv()
-        
-
-if __name__ == '__main__':
-    scraper = ZillowScraper()
-    scraper.run()
 
 
 
